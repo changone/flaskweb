2da89ecf36aadb175702ba49815acd7a8d8d711f
zypworkubuntu <zzzypp@163.com>
2018-08-23 16:21:47 +0800

    demo for jinjia2 Template Inheritance; add python requirement.txt

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
+# encoding=utf-8
+
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
 
-@app.before_request
-def before_request():
-    print 'before_request'
+# @app.before_request
+# def before_request():
+#     print 'before_request'
 
 
 @app.route('/')
 def index():
-    print 'request index'
-    return render_template('hello.html')
+    return render_template('base.html')
 
 
 @app.route('/home/<name>')
 def home(name):
-    name = '<h1>test</h1>'
     return render_template('hello.html', name=name)
 
 
-@app.after_request
-def after_request(response):
-    print 'after_request'
-    return response
+@app.route('/home/test')
+def test():
+    return render_template('index.html')
+
+# @app.after_request
+# def after_request(response):
+#     print 'after_request'
+#     return response
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
