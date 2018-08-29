dcd7d584c31fbe4b302d13ef8330bad3fa667867
zyphomepc <zzzypp@163.com>
2018-08-30 07:52:51 +0800

    create login form and show login page demo;

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from forms.LoginForm import LoginForm
 
 app = Flask(__name__, template_folder='templates')
 bootstrap = Bootstrap(app)
-
+app.config['SECRET_KEY'] = 'hardtoguessstring'
 # @app.before_request
 # def before_request():
 #     print 'before_request'
@@ -24,6 +25,18 @@ def home(name):
 @app.route('/home/test')
 def test():
     return render_template('test.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.email.data).first()
+    # if user is not None and user.verify_password(form.password.data):
+    #     login_user(user, form.remember_me.data)
+    #     return redirect(request.args.get('next') or url_for('main.index'))
+    # flash('Invalid username or password.')
+    return render_template('auth/index.html', form=form)
+
 
 # @app.after_request
 # def after_request(response):
